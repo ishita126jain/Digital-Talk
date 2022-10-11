ccd01281eebcae4ee111efc8e9db0d7f714f635c
ishita126jain <ishita126jain@gmail.com>
2022-10-11 19:00:13 +0530

    one more

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ const closeNavBtn = document.querySelector('#close__nav-btn');
 
 const openNav = () => {
     navItems.style.display = 'flex';
-    
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+
+}
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+
 }
 
 openNavBtn.addEventListener('click',openNav);
+closeNavBtn.addEventListener('click',closeNav);
